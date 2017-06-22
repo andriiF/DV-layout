eb5d8f544db38c047f23b4b3ab0594cb144daa9b
andriiF <andrii.fedorenko@batusystems.pl>
2017-06-22 12:40:09 +0200

    poprawka skryptu

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -1,21 +1,21 @@
 window.onload = function () {
     var elem = document.getElementsByClassName("toogleList");
     elem[0].style.height = "0";
-    if (screen.width <= 640) {
-        slides = document.querySelectorAll('#slides .slide');
-        idicators = document.querySelectorAll('#slides .indicators .fa-circle-o');
-        currentSlide = 0;
-        slideInterval = setInterval(nextSlide, 200000);
-    }
+    slides = document.querySelectorAll('#slides .slide');
+    idicators = document.querySelectorAll('#slides .indicators .fa-circle-o');
+    currentSlide = 0;
+    slideInterval = setInterval(nextSlide, 5000);
 
 
 }
 function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    idicators[currentSlide].className = 'fa fa-circle-o';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].className = 'slide showing';
-    idicators[currentSlide].className = 'fa fa-circle-o fa-circle';
+    if (screen.width <= 640) {
+        slides[currentSlide].className = 'slide';
+        idicators[currentSlide].className = 'fa fa-circle-o';
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].className = 'slide showing';
+        idicators[currentSlide].className = 'fa fa-circle-o fa-circle';
+    }
 
 }
 function previousSlide() {
